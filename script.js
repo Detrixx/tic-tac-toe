f9f7597e9a3e504f7529170b515b3aa4042dabc1
Vojtěch Kubíček <vojtechkubicek1@post.cz>
2023-04-04 17:08:31 +0200

    Update

---
+++ script.js
@@ -0,0 +1,26 @@
+let pole = [];
+let objekt;
+const bunka = document.querySelectorAll(".sloupec");
+
+for(let x=0;x<5;x++){
+    pole[x]=[];
+    for(let y=0;y<5;y++){
+        pole[x][y]= ["O"];
+        let bunkaSloupce = document.getElementsByClassName('radek'+x)[0];
+        let bunka = bunkaSloupce.querySelector(".sloupec"+y);
+        bunka.textContent = pole[x][y];
+    }
+}
+zacatekHry();
+
+function zacatekHry(){
+
+    bunka.forEach(bunka => bunka.addEventListener("click"),kliknutiNaBunku());
+        
+}
+function kliknutiNaBunku(){
+
+}
+
+
+console.log(pole);
